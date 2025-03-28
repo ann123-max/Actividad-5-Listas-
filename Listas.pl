7d4af11e299dc9ee4069d6e3549a64d058a3954a
ann123-max <ana.cabrales@utp.edu.co>
2025-03-27 21:11:32 -0500

    Create Listas.pl
    Se añadieron los hechos en forma de listas

---
+++ Listas.pl
@@ -0,0 +1,15 @@
+%HECHOS:
+
+%Padres:
+
+padre_de(abraham, [homero, herbert]).
+padre_de(clancy, [marge, patty, selma]).
+padre_de(homero, [bart, lisa, maggie]).
+
+%Madres:
+
+madre_de(mona, [homero]).
+madre_de(marge, [bart, lisa, maggie]).
+madre_de(jacqueline, [marge, patty, selma]).
+
+madre_de(selma, [ling]).
